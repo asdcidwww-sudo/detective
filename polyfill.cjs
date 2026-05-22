4434445423ebc984962dc7bfd8d0ded131fd98c9
asdcidwww <asdcidwww@gmail.com>
2026-05-22 16:47:47 +0800

    fix error

---
+++ polyfill.cjs
@@ -0,0 +1,18 @@
+const crypto = require('crypto');
+
+// Polyfill the native crypto module object itself
+if (!crypto.getRandomValues) {
+  crypto.getRandomValues = function (arr) {
+    return crypto.randomFillSync(arr);
+  };
+}
+
+// Also polyfill globalThis.crypto and getRandomValues
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = crypto.webcrypto || {
+    getRandomValues: crypto.getRandomValues
+  };
+} else if (typeof globalThis.crypto.getRandomValues === 'undefined') {
+  globalThis.crypto.getRandomValues = crypto.getRandomValues;
+}
+
